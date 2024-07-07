24d5d20f36fdfa201ae56929077c8195db199421
dankelley <kelley.dan@gmail.com>
2024-07-07 09:40:16 -0300

    Get localleader working

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,8 @@
 -- Add any additional options here
 vim.g.lazyvim_python_lsp = "pyright"
 vim.g.lazyvim_python_fuff = "ruff_lsp"
+vim.o.tabstop = 4
+vim.o.expandtab = true
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.g.maplocalleader = ","
--- lua/plugins/myplugins.lua
+++ lua/plugins/myplugins.lua
@@ -3,7 +3,23 @@ return {
   {
     "R-nvim/R.nvim",
     lazy = false,
-    R_assign = 2,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "markdown", "markdown_inline", "r", "rnoweb" },
+      })
+    end,
+  },
+  "R-nvim/cmp-r",
+  {
+    "hrsh7th/nvim-cmp",
+    filetypes = { "r", "rmd", "quarto", "rnoweb", "rhelp" },
+    config = function()
+      require("cmp").setup({ sources = { { name = "cmp_r" } } })
+      require("cmp_r").setup({})
+    end,
   },
 }
-
