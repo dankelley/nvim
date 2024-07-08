493c62dfb1d1273339170a50854bb6a9130333f5
dankelley <kelley.dan@gmail.com>
2024-07-08 16:17:53 -0300

    increase time limit for code formatting

--- lua/plugins/myplugins.lua
+++ lua/plugins/myplugins.lua
@@ -1,25 +1,26 @@
 return {
-  -- add R-nvim
-  {
-    "R-nvim/R.nvim",
-    lazy = false,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "markdown", "markdown_inline", "r", "rnoweb" },
-      })
-    end,
-  },
-  "R-nvim/cmp-r",
-  {
-    "hrsh7th/nvim-cmp",
-    filetypes = { "r", "rmd", "quarto", "rnoweb", "rhelp" },
-    config = function()
-      require("cmp").setup({ sources = { { name = "cmp_r" } } })
-      require("cmp_r").setup({})
-    end,
-  },
+    -- R support
+    {
+        "R-nvim/R.nvim",
+        lazy = false,
+    },
+    "R-nvim/cmp-r",
+    {
+        "hrsh7th/nvim-cmp",
+        filetypes = { "r", "rmd", "quarto", "rnoweb", "rhelp" },
+        config = function()
+            require("cmp").setup({ sources = { { name = "cmp_r" } } })
+            require("cmp_r").setup({})
+        end,
+    },
+    -- treesitter
+    {
+        "nvim-treesitter/nvim-treesitter",
+        run = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = { "markdown", "markdown_inline", "r", "rnoweb" },
+            })
+        end,
+    },
 }
