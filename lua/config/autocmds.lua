c69a293a9d72db75374ac4e30d7a58606628e56b
dankelley <kelley.dan@gmail.com>
2024-07-17 18:08:38 -0300

    source

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,6 +2,7 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+-- https://www.lazyvim.org/configuration/tips#disable-autoformat-for-some-buffers
 vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = { "R" },
   callback = function()
