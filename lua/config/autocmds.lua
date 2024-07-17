84120ab7b3a41dfaf2ea3dbb5fce41c118d495fb
dankelley <kelley.dan@gmail.com>
2024-07-17 18:13:33 -0300

    is it r or R?

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -4,7 +4,7 @@
 
 -- https://www.lazyvim.org/configuration/tips#disable-autoformat-for-some-buffers
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "R" },
+  pattern = { "r" },
   callback = function()
     vim.b.autoformat = false
   end,
