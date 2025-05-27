95751a572c7d276625a96475c3e39cc8cd3bb2b6
dankelley <kelley.dan@gmail.com>
2025-05-27 08:50:32 -0300

    did this break R formatting?

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,58 +1,109 @@
 if true then return {} end
--- A return {
--- A     {
--- A         "saghen/blink.cmp",
--- A         opts = {
--- A             -- snippets = {
--- A             --     expand = function(snippet, _)
--- A             --         return LazyVim.cmp.expand(snippet)
--- A             --     end,
--- A             -- },
--- A             -- appearance = {
--- A             --     -- sets the fallback highlight groups to nvim-cmp's highlight groups
--- A             --     -- useful for when your theme doesn't support blink.cmp
--- A             --     -- will be removed in a future release, assuming themes add support
--- A             --     use_nvim_cmp_as_default = false,
--- A             --     -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
--- A             --     -- adjusts spacing to ensure icons are aligned
--- A             --     nerd_font_variant = "mono",
--- A             -- },
--- A             -- completion = {
--- A             --     accept = {
--- A             --         -- experimental auto-brackets support
--- A             --         auto_brackets = {
--- A             --             enabled = true,
--- A             --         },
--- A             --     },
--- A             --     menu = {
--- A             --         draw = {
--- A             --             treesitter = { "lsp" },
--- A             --         },
--- A             --     },
--- A             --     documentation = {
--- A             --         auto_show = true,
--- A             --         auto_show_delay_ms = 200,
--- A             --     },
--- A             --     ghost_text = {
--- A             --         enabled = vim.g.ai_cmp,
--- A             --     },
--- A             -- },
--- A             --
--- A             -- -- experimental signature help support
--- A             -- -- signature = { enabled = true },
--- A             --
--- A             -- sources = {
--- A             --     -- adding any nvim-cmp sources here will enable them
--- A             --     -- with blink.compat
--- A             --     compat = {},
--- A             --     default = { "lsp", "path", "snippets", "buffer" },
--- A             --     cmdline = {},
--- A             -- },
--- A
--- A             keymap = {
--- A                 preset = "default",
--- A                 ["<TAB>"] = { "select_and_accept" },
--- A             },
--- A         }
--- A     }
--- A }
+
+-- return {
+--     "saghen/blink.cmp",
+--     opts = {
+--         snippets = {
+--             expand = function(snippet, _)
+--                 return LazyVim.cmp.expand(snippet)
+--             end,
+--         },
+--         appearance = {
+--             -- sets the fallback highlight groups to nvim-cmp's highlight groups
+--             -- useful for when your theme doesn't support blink.cmp
+--             -- will be removed in a future release, assuming themes add support
+--             use_nvim_cmp_as_default = false,
+--             -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+--             -- adjusts spacing to ensure icons are aligned
+--             nerd_font_variant = "mono",
+--         },
+--         completion = {
+--             accept = {
+--                 -- experimental auto-brackets support
+--                 auto_brackets = {
+--                     enabled = true,
+--                 },
+--             },
+--             menu = {
+--                 draw = {
+--                     treesitter = { "lsp" },
+--                 },
+--             },
+--             documentation = {
+--                 auto_show = true,
+--                 auto_show_delay_ms = 200,
+--             },
+--             ghost_text = {
+--                 enabled = vim.g.ai_cmp,
+--             },
+--         },
+--
+--         -- experimental signature help support
+--         -- signature = { enabled = true },
+--
+--         sources = {
+--             -- adding any nvim-cmp sources here will enable them
+--             -- with blink.compat
+--             compat = {},
+--             default = { "lsp", "path", "snippets", "buffer", "copilot" },
+--             -- default = { "lsp", "path", "snippets", "buffer" },
+--             providers = {
+--                 lsp = {
+--                     name = "lsp",
+--                     enabled = true,
+--                     module = "blink.cmp.sources.lsp",
+--                     kind = "LSP",
+--                     -- When linking markdown notes, I would get snippets and text in the
+--                     -- suggestions, I want those to show only if there are no LSP
+--                     -- suggestions
+--                     --
+--                     -- Enabled fallbacks as this seems to be working now
+--                     -- Disabling fallbacks as my snippets wouldn't show up when editing
+--                     -- lua files
+--                     -- fallbacks = { "snippets", "buffer" },
+--                     score_offset = 100, -- the higher the number, the higher the priority
+--                 },
+--                 path = {
+--                     name = "Path",
+--                     module = "blink.cmp.sources.path",
+--                     score_offset = 50,
+--                     -- When typing a path, I would get snippets and text in the
+--                     -- suggestions, I want those to show only if there are no path
+--                     -- suggestions
+--                     fallbacks = { "snippets", "buffer" },
+--                     opts = {
+--                         trailing_slash = false,
+--                         label_trailing_slash = true,
+--                         get_cwd = function(context)
+--                             return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
+--                         end,
+--                         show_hidden_files_by_default = true,
+--                     },
+--                 },
+--                 buffer = {
+--                     name = "Buffer",
+--                     enabled = true,
+--                     max_items = 3,
+--                     module = "blink.cmp.sources.buffer",
+--                     min_keyword_length = 4,
+--                     score_offset = 25, -- the higher the number, the higher the priority
+--                 },
+--                 copilot = {
+--                     name = "copilot",
+--                     enabled = true,
+--                     module = "blink-cmp-copilot",
+--                     kind = "Copilot",
+--                     min_keyword_length = 3,
+--                     score_offset = 25, -- the higher the number, the higher the priority
+--                     async = true,
+--                 },
+--             },
+--             -- cmdline = {},
+--         },
+--
+--         keymap = {
+--             preset = "default",
+--             ["<TAB>"] = { "select_and_accept" },
+--         },
+--     },
+-- }
