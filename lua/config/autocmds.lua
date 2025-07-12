1805d4c83c63f75d5923a7d84cf439a4e2810b4f
dankelley <kelley.dan@gmail.com>
2025-07-12 08:53:03 -0300

    various nvim

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -5,9 +5,20 @@
 -- BLOCK 1 START
 -- https://www.lazyvim.org/configuration/tips#disable-autoformat-for-some-buffers
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "r" },
-  callback = function()
-    vim.b.autoformat = false
-  end,
+    pattern = { "r" },
+    callback = function()
+        vim.b.autoformat = false
+    end,
 })
 -- BLOCK 1 END
+
+-- BLOCK 2 START
+-- https://github.com/Integralist/nvim/blob/main/lua%2Fautocommands.lua
+-- always open quickfix window automatically.
+-- this uses cwindows which will open it only if there are entries.
+vim.api.nvim_create_autocmd("QuickFixCmdPost", {
+    group = vim.api.nvim_create_augroup("AutoOpenQuickfix", { clear = true }),
+    pattern = { "[^l]*" },
+    command = "cwindow"
+})
+-- BLOCK 2 END
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,4 @@
 vim.keymap.set("n", "<F8>", "14j")
 vim.keymap.set("n", "<F7>", "14k")
 vim.keymap.set("i", "jj", "<ESC>", { silent = true })
+vim.keymap.set("n", "<leader>h", Snacks.dashboard.open, { desc = "Open mini starter" })
